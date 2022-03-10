e0a8ed86c3fcfd806a60ab944ce50fc10fe79297
Eliana Kern <Eliana Kern>
2022-03-10 12:42:11 -0300

    estilo componetes funcionales, ListMovie y Item

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,21 @@
+import '../styles/components-styles/_Item.scss';
 import { Link } from 'react-router-dom';
-const Item = ({img, titleItem, id}) => {
-    return (
-        <div>
-            <div><img src={img}/></div>
-            <Link to={`/movie/${id}`}>
-            <p>{titleItem}</p>
-            <i>icono flecha</i>
-            </Link>
+import { GrNext } from "react-icons/gr";
+
+const Item = ({ img, titleItem, id }) => {
+  return (
+      <li className='items'>
+        <div className="img-item">
+          <img src={img} />
         </div>
-    )
-}
+        <Link className="titulo-item" to={`/movie/${id}`}>
+          <p>{titleItem}</p>
+        </Link>
+        <Link to={`/movie/${id}`}>
+          <GrNext className='next'/>
+        </Link>
+        
+      </li>
+  );
+};
 export default Item;
