b18f2e17df83583852f7b32dbb88c6b0b1f20873
Eliana Kern <Eliana Kern>
2022-03-18 23:04:11 -0300

    armo y estilo componente footer y agrego funciones auxiliares

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import '../styles/components-styles/_Footer.scss';
+import { hrefMail, hrefLinkedin, hrefGitHub } from './Auxiliares';
 import { FaRegEnvelope } from 'react-icons/fa';
-import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
 
 const Footer = () => {
   return (
@@ -8,23 +9,23 @@ const Footer = () => {
       <p>Hecho por Eliana Kern</p>
       <ul className="contacto-footer">
         <li>
-          <a href="">
-            <FaRegEnvelope />
+          <a href={hrefGitHub}>
+            <AiFillGithub className="icono-footer" />
           </a>
         </li>
         <li>
-          <a href="">
-              <AiFillGithub/>
+          <a href={hrefMail}>
+            <FaRegEnvelope className="icono-footer" />
           </a>
         </li>
         <li>
-          <a href="">
-              <AiFillLinkedin/>
+          <a href={hrefLinkedin}>
+            <AiFillLinkedin className="icono-footer" />
           </a>
         </li>
       </ul>
     </footer>
-  )
+  );
 };
 
 export default Footer;
