ed356f32f499c7e3cba6d605b97acda81259d1a2
Eliana Kern <Eliana Kern>
2022-03-22 01:22:03 -0300

    armo el fetch y el retorno del componente Search, agrego algo de estilado

--- src/components/ListMovie.js
+++ src/components/ListMovie.js
@@ -2,6 +2,7 @@ import '../styles/components-styles/_ListMovie.scss';
 import { urlBaseImg, tipoMovie } from '../components/Auxiliares';
 import Item from '../components/Item';
 import UseFetch from '../hooks/UseFetch';
+import NoDisponible from "../assets/no-disponible.png"
 
 const ListMovie = ({ title, categoria }) => {
   const { results: peliculas = [] } = UseFetch(tipoMovie, categoria);
@@ -14,8 +15,8 @@ const ListMovie = ({ title, categoria }) => {
       <ul className="list">
         {peliculas.map((pelicula) => (
           <Item
-            key={pelicula.id}
-            img={`${urlBaseImg}w200/${pelicula.poster_path}`}
+            key={pelicula.id} //no me esta funcionando la img no disponible!!//
+            img={pelicula.poster_path!== null ?`${urlBaseImg}w200/${pelicula.poster_path}`: {NoDisponible} }
             titleItem={pelicula.title}
             id={pelicula.id}
           />
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,21 @@
 import '../styles/components-styles/_Search.scss';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
+import {
+  baseUrl,
+  search,
+  tipoMovie,
+  query,
+  apiKey,
+  lenguaje,
+  page,
+} from './Auxiliares';
+import PresentacionTarjetas from './PresentacionTarjetas';
+import { FaSearch } from "react-icons/fa";
 
 const Search = () => {
   const [valorDelInput, setValorDelInput] = useState('');
+  const [resultados, setResultados] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams({
     query: '',
   });
@@ -16,14 +28,33 @@ const Search = () => {
     setSearchParams({ query: valorDelInput });
   };
 
+  useEffect(() => {
+    if (searchParams.get('query')) {
+      fetch(
+        `${baseUrl}/${search}/${tipoMovie}?${query}=${searchParams.get(
+          'query'
+        )}&${apiKey}&${lenguaje}=es-AR&${page}=1`
+      )
+        .then((res) => res.json())
+        .then((data) => setResultados(data.results));
+    }
+  }, [searchParams]);
+
+  console.log(resultados);
   return (
-    <div>
-      Busqueda
-      <form onSubmit={handleSubmit}>
-        <input type="tex" onChange={handleChange} />
-        <input type="submit" value="Buscar" />
-      </form>
-    </div>
+    <section className="seccion-busqueda">
+      <div className='busqueda'>
+        <h2>Busca tu Pelicula</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" onChange={handleChange} value={valorDelInput} />
+          <input type="submit" value="Buscar" /><FaSearch className='lupa'/>
+        </form>
+      </div>
+      <PresentacionTarjetas
+        titulo="Resultados de Busqueda"
+        peliculas={resultados}
+      />
+    </section>
   );
 };
 export default Search;
