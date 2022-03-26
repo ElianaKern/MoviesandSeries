698c8c3a37918af2593637b006c804f93fac9bca
Eliana Kern <Eliana Kern>
2022-03-25 23:49:21 -0300

    agrego componente Contexto y lo uso para agregar al nav, ocion de ver seres o peliculas, aun no termina de funcionar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.scss"
+import './App.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import Home from './components/Home';
@@ -6,22 +6,29 @@ import NewMovies from './components/NewMovies';
 import PopularMovies from './components/PopularMovies';
 import Search from './components/Search';
 import DescripcionMovie from './components/DescripcionMovie';
-import DescripcionActor from "./components/DescripcionActor";
-import Footer from "./components/Footer";
+import DescripcionActor from './components/DescripcionActor';
+import Footer from './components/Footer';
+import Contexto from './contexto/Contexto';
+import { useState } from 'react';
 
 const App = () => {
+  const [tipo, setTipo] = useState("movie");
+  const contexto = { tipo: tipo, setTipo: setTipo };
+  console.log(contexto)
   return (
     <BrowserRouter>
-      <Nav></Nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movie-new" element={<NewMovies />} />
-        <Route path="/movie-popular" element={<PopularMovies />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/movie/:id" element={<DescripcionMovie />} />
-        <Route path="/actor/:id" element={<DescripcionActor />} />
-      </Routes>
-      <Footer></Footer>
+      <Contexto.Provider value={contexto}>
+        <Nav></Nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie-new" element={<NewMovies />} />
+          <Route path="/movie-popular" element={<PopularMovies />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/movie/:id" element={<DescripcionMovie />} />
+          <Route path="/actor/:id" element={<DescripcionActor />} />
+        </Routes>
+        <Footer></Footer>
+      </Contexto.Provider>
     </BrowserRouter>
   );
 };
--- src/hooks/UseFetch.js
+++ src/hooks/UseFetch.js
@@ -1,14 +1,17 @@
 import { useState, useEffect } from 'react';
 import { baseUrl, apiKey, lenguaje, page } from '../components/Auxiliares';
+import { useContext } from 'react';
+import Contexto from "../contexto/Contexto";
 
 const UseFetch = (tipo, categoria) => {
+  const contexto = useContext(Contexto);
   const [data, setData] = useState({});
 
   useEffect(() => {
-    fetch(`${baseUrl}/${tipo}/${categoria}?${apiKey}&${lenguaje}=es-AR&${page}=1`)
+    fetch(`${baseUrl}/${contexto.tipo}/${categoria}?${apiKey}&${lenguaje}=es-AR&${page}=1`)
       .then((res) => res.json())
       .then((dataRes) => setData(dataRes));
-  }, []);
+  }, [contexto]);
 
   return data;
 };
